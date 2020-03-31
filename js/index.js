42edc75050ad0ee0b146cee1a6865c15c4fdf6f9
zironyx <62961603+zironyx@users.noreply.github.com>
2020-04-01 00:52:56 +0530

    Initial for hosting

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// select2
+$(function () {
+    $('select').each(function () {
+        $(this).select2({
+            theme: 'bootstrap4',
+            width: 'style',
+            placeholder: $(this).attr('placeholder'),
+            allowClear: Boolean($(this).data('allow-clear')),
+        });
+    });
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
